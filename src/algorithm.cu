682755dab429cfa9615c216c515aac419c540fd8
Kelly Davis <kdavis@mozilla.com>
2015-06-07 17:30:59 +0200

    Resolved issue #2 exchanging use of tmpnam for mkstemp

--- src/algorithm.cu
+++ src/algorithm.cu
@@ -7,7 +7,7 @@
 //
 
 
-#include <cstdio>
+#include <stdlib.h>
 #include <iostream>
 
 #include <cusp/io/matrix_market.h>
@@ -24,8 +24,11 @@ namespace rtrl
 
     void Algorithm::save()
     {
+        // Define template weight matrix filename
+        char filename[] ="weight_matrix_XXXXXX";
+
         // Obtain unique weight matrix filename
-        const char *filename = tmpnam(0);
+        mkstemp(filename);
 
         // Write weght matrix to filename
         cusp::io::write_matrix_market_file(m_arguments.get_weight_matrix(),filename);
